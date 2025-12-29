1c73c2dd32cb97ff719b3be1843eb0e39547baf2
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-12-29 16:31:36 +0100

    enhance navigation and styling for mobile responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,16 +16,32 @@ export default function Nav() {
     return (
         <div className="nav-bar">
             <nav className="navigation">
-                <ul>
+                <ul className="nav">
+                    <li className="toggle">
+                        <span className="uno"></span>
+                        <span className="dos"></span>
+                    </li>
+                    <li className="toggle mobile" onClick={toggleMenu}>
+                        <span className="uno"></span>
+                        <span className="dos"></span>
+                    </li>
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Store</li>
                 </ul>
-                <h1 className="logo">EFRONA</h1>
-                <ul>
+                <ul className={`hamburger extras ${showMenu ? "show" : ""}`}>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Store</li>
                     <button>Contact Us</button>
-                    <li>C</li>
                 </ul>
+
+                <h1 className="logo">EFRONA</h1>
+                <ul className="extras">
+                    <button>Contact Us</button>
+                    <li><ion-icon name="cart-outline"></ion-icon></li>
+                </ul>
+                
             </nav>
         </div>
     )
